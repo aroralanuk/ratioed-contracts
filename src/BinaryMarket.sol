54b80bd09dd7aaa6bffe2cae30cc627b893c14d5
-f <kunalarora1729@gmail.com>
2024-06-28 03:56:47 +0530

    virtual liquidity

--- src/BinaryMarket.sol
+++ src/BinaryMarket.sol
@@ -2,15 +2,26 @@
 pragma solidity ^0.8.13;
 
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+import {ERC1155} from "@openzeppelin/token/ERC1155/ERC1155.sol";
+import {ERC1155Supply} from "@openzeppelin/token/ERC1155/extensions/ERC1155Supply.sol";
 import {Math} from "@openzeppelin/utils/math/Math.sol";
 import {SD59x18 as FixedMath, exp, sd, log2, unwrap, wrap} from "@prb/math/SD59x18.sol";
 
-contract BinaryMarket {
+contract BinaryMarket is ERC1155Supply {
     using Math for uint256;
 
+    event LiquidityAdded(address indexed provider, uint256 amount);
     event Swapped(address indexed account, int256 q0, int256 q1, int256 cost);
+    event MarketClosed(bool outcome);
+    event TokensRedeemed(address indexed account, uint256 amount);
 
     int256 private constant LOG_2 = 699502064000000000; // log2(2) * 1e18
+    uint256 public constant YES_TOKEN_ID = 0;
+    uint256 public constant NO_TOKEN_ID = 1;
+    uint256 public constant VIRTUAL_LIQUIDITY = 1000;
+
+    bool public isClosed;
+    bool public finalOutcome;
     uint256[2] public shares; // Outstanding shares for each outcome (Yes/No)
     uint256 public liquidityParam;
 
@@ -18,11 +29,27 @@ contract BinaryMarket {
     IERC20 public outcomeTokenYes;
     IERC20 public outcomeTokenNo;
 
-    constructor(IERC20 _collateralToken) {
+    constructor(IERC20 _collateralToken) ERC1155("") {
         collateralToken = _collateralToken;
+        isClosed = false;
     }
 
+    // function addLiquidity(uint256 amount) public {
+    //     require(amount > 0, "Amount must be greater than 0");
+
+    //     require(collateralToken.transferFrom(msg.sender, address(this), amount), "Collateral transfer failed");
+
+    //     uint256 tokensToMint = amount / 2;
+    //     _mint(address(this), YES_TOKEN_ID, tokensToMint, "");
+    //     _mint(address(this), NO_TOKEN_ID, tokensToMint, "");
+
+    //     liquidityParam += amount;
+
+    //     emit LiquidityAdded(msg.sender, amount);
+    // }
+
     function swap(int256 q0, int256 q1, int256 maxPrice) public returns (int256 cost) {
+        require(!isClosed, "Market is closed");
         cost = calculateCost(q0, q1);
         // do something
         require(maxPrice == 0 || cost <= maxPrice, "Cost exceeds limit");
@@ -38,25 +65,52 @@ contract BinaryMarket {
 
         // yes
         if (q0 > 0) {
-            require(outcomeTokenYes.transfer(msg.sender, uint256(q0)), "Yes token transfer failed");
+            _safeTransferFrom(address(this), msg.sender, YES_TOKEN_ID, uint256(q0), "");
         } else if (q0 < 0) {
-            require(outcomeTokenYes.transferFrom(msg.sender, address(this), uint256(-q0)), "Yes token receive failed");
+            _safeTransferFrom(msg.sender, address(this), YES_TOKEN_ID, uint256(-q0), "");
         }
 
         // no
         if (q1 > 0) {
-            require(outcomeTokenNo.transfer(msg.sender, uint256(q1)), "No token transfer failed");
+            _safeTransferFrom(address(this), msg.sender, NO_TOKEN_ID, uint256(q1), "");
         } else if (q1 < 0) {
-            require(outcomeTokenNo.transferFrom(msg.sender, address(this), uint256(-q1)), "No token receive failed");
+            _safeTransferFrom(msg.sender, address(this), NO_TOKEN_ID, uint256(-q1), "");
         }
 
         emit Swapped(msg.sender, q0, q1, cost);
     }
 
+    // TODO: access control
+    function close(bool outcome) external {
+        require(!isClosed, "Market is already closed");
+        isClosed = true;
+        finalOutcome = outcome;
+        emit MarketClosed(outcome);
+    }
+
+    function redeemTokens() external {
+        require(isClosed, "Market is not closed yet");
+
+        uint256 winningTokenId = finalOutcome ? YES_TOKEN_ID : NO_TOKEN_ID;
+        uint256 tokenBalance = balanceOf(msg.sender, winningTokenId);
+
+        require(tokenBalance > 0, "No winning tokens to redeem");
+
+        uint256 redeemAmount = tokenBalance;
+
+        // Burn the winning tokens
+        _burn(msg.sender, winningTokenId, tokenBalance);
+
+        // Transfer collateral tokens to the user
+        require(collateralToken.transfer(msg.sender, redeemAmount), "Collateral transfer failed");
+
+        emit TokensRedeemed(msg.sender, redeemAmount);
+    }
+
     function calculateCost(int256 q0, int256 q1) public view returns (int256 cost) {
         int256[] memory otExpNums = new int256[](2);
-        otExpNums[0] = q0 + int256(outcomeTokenYes.balanceOf(address(this)));
-        otExpNums[1] = q1 + int256(outcomeTokenNo.balanceOf(address(this)));
+        otExpNums[0] = q0 + int256(totalSupply(YES_TOKEN_ID) + VIRTUAL_LIQUIDITY);
+        otExpNums[1] = q1 + int256(totalSupply(NO_TOKEN_ID) + VIRTUAL_LIQUIDITY);
 
         (int256 sum, int256 offset) = sumExpOffset(otExpNums);
         cost = unwrap(log2(wrap(sum))) + offset;
--- test/BinaryMarket.t.sol
+++ test/BinaryMarket.t.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 
-contract RatioedTest is Test {}
+import {BinaryMarket} from "src/BinaryMarket.sol";
+import {MockERC20} from "forge-std/mocks/MockERC20.sol";
+
+contract BinaryMarketTest is Test {
+    BinaryMarket public market;
+    MockERC20 public collateralToken;
+
+    address public alice = address(0x1);
+    address public bob = address(0x2);
+    address public charlie = address(0x3);
+
+    uint256 public constant INITIAL_LIQUIDITY = 1000000 * 1e18;
+}
